ab72e5a561649a0b08da7ffe875859cc81a4ed9f
alviandk <alviandk@gmail.com>
2015-07-13 17:28:23 +0700

    taskbuster setengah part 3

--- FindTheMentor/settings/base.py
+++ FindTheMentor/settings/base.py
@@ -63,13 +63,17 @@ ROOT_URLCONF = 'FindTheMentor.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,"templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+		'django.contrib.auth.context_processors.auth',
                 'django.template.context_processors.debug',
+		'django.template.context_processors.i18n',
+		'django.template.context_processors.media',
+		'django.template.context_processors.static',
+		'django.template.context_processors.tz',
                 'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
@@ -106,5 +110,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
+STATICFILES_DIRS=(
+	os.path.join(BASE_DIR,"static"),
+)
 STATIC_URL = '/static/'
