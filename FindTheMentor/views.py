f34cb8bd557c3d198366b687f8c22b7291e1bb51
alviandk <alviandk@gmail.com>
2015-07-14 18:27:34 +0700

    taskbuster part 4

--- FindTheMentor/views.py
+++ FindTheMentor/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, "FindTheMentor/index.html", {})
+
+def home_files(request, filename):
+    return render(request, filename, {}, content_type="text/plain")
+
